b794494f0032b5a0c825e6bce286f0b2ae80bca6
Thejas Gowda M K <thejasgowdamk@Thejass-MacBook-Pro.local>
2022-08-19 15:26:32 +0530

    Deltax Entertainment

---
+++ Models/UI_data.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication17.Models
+{
+    public class UI_data
+    {
+        public DateTime Release_Date { get; set; }
+        public string Producer { get; set; }
+        public List<string> Actors { get; set; }
+        public string Movie_Name { get; set; }
+        public int id { get; set; }
+
+
+        public UI_data(string Movie_Name,DateTime Release_Date,string Producer,List<string> Actors,int id)
+        {
+            this.Movie_Name = Movie_Name;
+            this.Release_Date = Release_Date;
+            this.Producer = Producer;
+            this.Actors = Actors;
+            this.id = id;
+        }
+
+        public UI_data()
+        {
+        }
+    }
+}
---
+++ WebApplication17/Models/UI_data.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication17.Models
+{
+    public class Class1
+    {
+        public DateTime Release_Date { get; set; }
+        public string Producer { get; set; }
+        public List<string> Actors { get; set; }
+        public string Movie_Name { get; set; }
+
+
+        public Class1(string Movie_Name,DateTime Release_Date,string Producer,List<string> Actors)
+        {
+            this.Movie_Name = Movie_Name;
+            this.Release_Date = Release_Date;
+            this.Producer = Producer;
+            this.Actors = Actors;
+        }
+
+        public Class1()
+        {
+        }
+    }
+}
